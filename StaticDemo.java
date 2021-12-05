d0d25abef1f0c88ffea46caeedd1784badc7db1a
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-05 13:04:41 +0530

    ADDED: Using Static Keyword Program

---
+++ StaticDemo.java
@@ -0,0 +1,12 @@
+public class StaticDemo {
+    int a = 0;
+    public StaticDemo(){
+        a++;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {//start of program
+        StaticDemo s1 = new StaticDemo();
+        StaticDemo s2 = new StaticDemo();
+        StaticDemo s3 = new StaticDemo();
+    }
+}
